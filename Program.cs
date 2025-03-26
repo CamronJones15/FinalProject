32a01069b4cccc9ddf076b463db9bbc2944c5851
Fernando Rios-Cruz <146597448+FernRios@users.noreply.github.com>
2025-03-26 17:50:30 -0400

    Added source files from StarterGame frame work

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FinalProject
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+            game.Play();
+            game.End();
+        }
+    }
+}
